96a9124baa09153a3549f16f90e3e12361bd4f52
Kuldep Sahoo <sahookuldeep192@gmail.com>
2025-05-23 17:22:42 +0530

    first commit

---
+++ with-ip-op/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/App.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "output.js",
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3500,
+  },
+};
